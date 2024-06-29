853585fbdd5d2d0af440c805d629a025ef5545d9
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2024-06-29 00:12:55 -0700

    Update to ResearchKitOnFHIR 2.0 (#27)
    # Update to ResearchKitOnFHIR 2.0
    
    ## :recycle: Current situation & Problem
    - ResearchKitOnFHIR 1.4.0 was incorrectly tagged
    
    
    ## :gear: Release Notes 
    - Update to ResearchKitOnFHIR 2.0
    
    
    ## :pencil: Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordSpezi/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordSpezi/.github/blob/main/CONTRIBUTING.md).

--- Package.swift
+++ Package.swift
@@ -8,7 +8,15 @@
 // SPDX-License-Identifier: MIT
 //
 
+import class Foundation.ProcessInfo
 import PackageDescription
+
+
+#if swift(<6)
+let swiftConcurrency: SwiftSetting = .enableExperimentalFeature("SwiftConcurrency")
+#else
+let swiftConcurrency: SwiftSetting = .enableUpcomingFeature("SwiftConcurrency")
+#endif
 
 
 let package = Package(
@@ -26,8 +34,8 @@ let package = Package(
         .package(url: "https://github.com/StanfordSpezi/SpeziViews", from: "1.0.0"),
         .package(url: "https://github.com/apple/FHIRModels", .upToNextMinor(from: "0.5.0")),
         .package(url: "https://github.com/StanfordBDHG/ResearchKit", from: "3.0.1"),
-        .package(url: "https://github.com/StanfordBDHG/ResearchKitOnFHIR", from: "1.4.0")
-    ],
+        .package(url: "https://github.com/StanfordBDHG/ResearchKitOnFHIR", from: "2.0.0")
+    ] + swiftLintPackage(),
     targets: [
         .target(
             name: "SpeziQuestionnaire",
@@ -38,13 +46,21 @@ let package = Package(
                 .product(name: "FHIRQuestionnaires", package: "ResearchKitOnFHIR"),
                 .product(name: "ResearchKit", package: "ResearchKit"),
                 .product(name: "ResearchKitSwiftUI", package: "ResearchKit")
-            ]
+            ],
+            swiftSettings: [
+                swiftConcurrency
+            ],
+            plugins: [] + swiftLintPlugin()
         ),
         .testTarget(
             name: "SpeziQuestionnaireTests",
             dependencies: [
                 .target(name: "SpeziQuestionnaire")
-            ]
+            ],
+            swiftSettings: [
+                swiftConcurrency
+            ],
+            plugins: [] + swiftLintPlugin()
         ),
         .target(
             name: "SpeziTimedWalkTest",
@@ -52,7 +68,29 @@ let package = Package(
                 .product(name: "Spezi", package: "Spezi"),
                 .product(name: "SpeziViews", package: "SpeziViews"),
                 .product(name: "ModelsR4", package: "FHIRModels")
-            ]
+            ],
+            swiftSettings: [
+                swiftConcurrency
+            ],
+            plugins: [] + swiftLintPlugin()
         )
     ]
 )
+
+
+func swiftLintPlugin() -> [Target.PluginUsage] {
+    // Fully quit Xcode and open again with `open --env SPEZI_DEVELOPMENT_SWIFTLINT /Applications/Xcode.app`
+    if ProcessInfo.processInfo.environment["SPEZI_DEVELOPMENT_SWIFTLINT"] != nil {
+        [.plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint")]
+    } else {
+        []
+    }
+}
+
+func swiftLintPackage() -> [PackageDescription.Package.Dependency] {
+    if ProcessInfo.processInfo.environment["SPEZI_DEVELOPMENT_SWIFTLINT"] != nil {
+        [.package(url: "https://github.com/realm/SwiftLint.git", .upToNextMinor(from: "0.55.1"))]
+    } else {
+        []
+    }
+}
