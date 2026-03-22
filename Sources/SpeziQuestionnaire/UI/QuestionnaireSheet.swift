a064461c98431434261e1f56bdc6fa7a76cd5a6b
Lukas Kollmer <hey@lukaskollmer.de>
2026-03-22 21:37:10 +0100

    restructure files

--- Sources/SpeziQuestionnaire/UI/QuestionnaireSheet.swift
+++ Sources/SpeziQuestionnaire/UI/QuestionnaireSheet.swift
@@ -45,7 +45,6 @@ public import SwiftUI
 /// ```
 public struct QuestionnaireSheet: View {
     private let questionnaire: Questionnaire
-    private let customQuestionKinds: [any QuestionKindDefinition]
     private let completionStepConfig: CompletionStepConfig
     private let resultHandler: @MainActor (Result) async -> Void
     
@@ -73,7 +72,6 @@ public struct QuestionnaireSheet: View {
         }
         .accessibilityIdentifier("SpeziQuestionnaireNavStack")
         .environment(responses)
-        .environment(\.customQuestionKinds, customQuestionKinds)
     }
     
     /// Creates a new `QuestionnaireSheet`
@@ -89,20 +87,13 @@ public struct QuestionnaireSheet: View {
         _ questionnaire: Questionnaire,
         responses: QuestionnaireResponses? = nil,
         completionStepConfig: CompletionStepConfig = .enable,
-        additionalQuestionKinds: [any QuestionKindDefinition] = [],
         resultHandler: @escaping @MainActor (Result) async -> Void
     ) {
         self.questionnaire = questionnaire.withConditionsSimplified()
         self.completionStepConfig = completionStepConfig
         self.responses = responses ?? QuestionnaireResponses(questionnaire: questionnaire)
-        self.customQuestionKinds = additionalQuestionKinds
         self.resultHandler = resultHandler
     }
-}
-
-
-extension EnvironmentValues {
-    @Entry var customQuestionKinds: [any QuestionKindDefinition] = []
 }
 
 
